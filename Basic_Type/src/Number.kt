344637d0f2f8d0ebc709e6b235d0547c0e834ec5
Bhavesh Joshi <34483890+gokulbhaveshjoshi@users.noreply.github.com>
2021-02-12 21:37:37 +0530

    Basic number type

---
+++ Basic_Type/src/Number.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>){
+    val a = 1000
+    val d = 100.00
+    val f = 100.00f
+    val l:Long = 10000000045
+    val s:Short = 10
+    val b:Byte = 1
+
+    println("Your Int Value is $a")
+    println("Your double value is $d")
+    println("Your Float Value is $f")
+    println("Your Long value is $l")
+    println("Your Short Value is $s")
+    println("Your Byte value is $b")
+
+}
